5189f8718dba915f0cc54b7bef50c030f8529730
Khushi197 <113971416+Khushi197@users.noreply.github.com>
2023-02-13 12:37:25 +0530

    Create binary tree inorder traversal.cpp

---
+++ binary tree inorder traversal.cpp
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/897007587/
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        stack<TreeNode*> todo;
+        while (root || !todo.empty()) {
+            while (root) {
+                todo.push(root);
+                root = root -> left;
+            }
+            root = todo.top();
+            todo.pop();
+            nodes.push_back(root -> val);
+            root = root -> right;
+        }
+        return nodes;
+    
+        
+    }
+    
+};
